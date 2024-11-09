c4938ffd08f1e28716bb75994a32512297ce4f45
random20 <syachan3@gmail.com>
2024-11-09 10:30:39 +0700

    include username in database

--- ujianlabor.sql
+++ ujianlabor.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2022 at 05:30 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Nov 09, 2024 at 04:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `pendaftaran` (
   `id` int(10) NOT NULL,
+  `username` varchar(30) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `nisn` varchar(15) NOT NULL,
   `jurusan` varchar(30) NOT NULL,
@@ -36,15 +37,18 @@ CREATE TABLE `pendaftaran` (
   `nohp` varchar(13) NOT NULL,
   `alamat` varchar(50) NOT NULL,
   `ratanilai` double NOT NULL,
+  `ktp` text NOT NULL,
   `statuscs` enum('-','diterima','cadangan','tidakditerima') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pendaftaran`
 --
 
-INSERT INTO `pendaftaran` (`id`, `nama`, `nisn`, `jurusan`, `email`, `nohp`, `alamat`, `ratanilai`, `statuscs`) VALUES
-(5, 'nopi', '8765325421', 'RPL', 'nopi@gmail.com', '082134652123', 'Rajawali', 87.6, '-');
+INSERT INTO `pendaftaran` (`id`, `username`, `nama`, `nisn`, `jurusan`, `email`, `nohp`, `alamat`, `ratanilai`, `ktp`, `statuscs`) VALUES
+(5, '', 'nopi', '8765325421', 'RPL', 'nopi@gmail.com', '082134652123', 'Rajawali', 87.6, '', ''),
+(7, 'ezi', 'ezi', '34523134', 'Sistem Informasi', 'ezi@ezimail.com', '123123125123', 'asdasgasd', 89.8, 'uploads/672cf0193b066.jpeg', 'diterima'),
+(9, '2055201009', 'banu', '2055201009', 'teknik informatika', 'neufa404@gmail.com', '082170092103', 'jl. barito sari', 90, '../uploads/672e57446bb6f.jpg', '-');
 
 -- --------------------------------------------------------
 
@@ -56,17 +60,18 @@ CREATE TABLE `user` (
   `id` int(10) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
-  `level` enum('admin','calonsiswa') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role` enum('admin','calonsiswa') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
+INSERT INTO `user` (`id`, `username`, `password`, `role`) VALUES
 (1, 'admin', 'admin', 'admin'),
-(3, 'ezi', '12345', 'calonsiswa'),
-(4, '8765325421', '123', 'calonsiswa');
+(3, 'usernomor2', '12345', 'calonsiswa'),
+(4, '8765325421', '123', 'calonsiswa'),
+(5, '2055201009', '123', 'calonsiswa');
 
 --
 -- Indexes for dumped tables
@@ -76,7 +81,8 @@ INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
 -- Indexes for table `pendaftaran`
 --
 ALTER TABLE `pendaftaran`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`);
 
 --
 -- Indexes for table `user`
@@ -92,13 +98,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `pendaftaran`
 --
 ALTER TABLE `pendaftaran`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
